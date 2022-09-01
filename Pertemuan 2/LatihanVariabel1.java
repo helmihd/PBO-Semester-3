ee43e16e34a1020e7992c7df82fbe3d89ce0702d
helmihd <92964269+helmihd@users.noreply.github.com>
2022-09-01 15:03:09 +0700

    Add files via upload

---
+++ Pertemuan 2/LatihanVariabel1.java
@@ -0,0 +1,15 @@
+public class LatihanVariabel1 {
+    public static void main(String[] args) {
+        int myNum = 5; // Integer (whole number)
+        float myFloatNum = 5.99f; // Floating point number
+        char myLetter = 'D'; // Character
+        boolean myBoolean = true; // Boolean
+        String myText = "Hello"; // String
+        // print value
+        System.out.println("myNum = "+myNum);
+        System.out.println("myFloatNum = "+myFloatNum);
+        System.out.println("myLetter = "+myLetter);
+        System.out.println("myBoolean = "+myBoolean);
+        System.out.println("myText = "+myText);
+    }
+}

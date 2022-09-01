479e6efae3fa79209c1dbcf2e263fcef05a688fd
helmihd <92964269+helmihd@users.noreply.github.com>
2022-09-01 15:09:40 +0700

    Add files via upload

---
+++ Pertemuan 2/LatihanVariabel2.java
@@ -0,0 +1,15 @@
+public class LatihanVariabel2 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2 + 3;
+        int c = 9 / 2;
+        float d = 5;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(a+b);
+        c = 10;
+        System.out.println(c);
+    }
+}
